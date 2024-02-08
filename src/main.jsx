155fed255f595fd753e1c6ef6e63504f85d33016
Kelly Lee <kellyws.lee@gmail.com>
2024-02-07 22:36:23 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import AllProducts from './pages/AllProducts.jsx';
+import NewProduct from './pages/NewProduct.jsx';
+import ProductDetail from './pages/ProductDetail.jsx';
+import MyCart from './pages/MyCart.jsx';
+import NotFound from './pages/NotFound.jsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/products', element: <AllProducts /> },
+      { path: '/products/new', element: <NewProduct /> },
+      { path: '/products/:id', element: <ProductDetail /> },
+      { path: '/carts', element: <MyCart /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
