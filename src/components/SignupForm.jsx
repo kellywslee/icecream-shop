fee87d89715946671dc4e54da3a373b281499009
Kelly Lee <kellyws.lee@gmail.com>
2024-02-10 07:29:21 -0500

    Add confirm password in the signup form

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -7,7 +7,8 @@ export default function SignupForm() {
     register,
     handleSubmit,
     reset,
-    // formtate: { errors },
+    formState: { errors },
+    getValues,
   } = useForm({ mode: "onBlur" });
 
   const onSubmit = (data) => {
@@ -31,19 +32,36 @@ export default function SignupForm() {
         type="text"
         {...register("displayName", { required: "Username is required" })}
       />
+      {errors.displayName && <p>{errors.displayName.message}</p>}
       <label htmlFor="email">Email</label>
       <input
         type="email"
         {...register("email", { required: "Email is required" })}
       />
+      {errors.email && <p>{errors.email.message}</p>}
       <label htmlFor="password">Password</label>
       <input
         type="password"
         {...register("password", {
           required: "Password is required",
-          minLength: 6,
+          minLength: {
+            value: 6,
+            message: "Password must have at least 6 characters",
+          },
         })}
       />
+      {errors.password && <p>{errors.password.message}</p>}
+      <label htmlFor="confirmPassword">Confirm Password</label>
+      <input
+        type="password"
+        {...register("confirmPassword", {
+          validate: (value) => {
+            const { password } = getValues();
+            return value === password || "Passwords do not match";
+          },
+        })}
+      />
+      {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
       {!isLoading && <button type="submit">sign up</button>}
       {isLoading && <button disabled>loading</button>}
       {error && <p>{error.message}</p>}
