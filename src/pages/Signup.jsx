30618c063b85048b55210196303b3df91061a245
Kelly Lee <76071382+kellywslee@users.noreply.github.com>
2024-02-13 17:26:02 -0500

    styles: Update css in the signup page (#2)
    * Rename SignUp.jsx to Signup.jsx
    
    * styles: Update css in the signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import SignupForm from "../components/SignupForm";
 
 export default function Signup() {
   return (
-    <main className="flex w-full flex-col items-center gap-6 pt-24 font-rubik">
+    <main className="gap-6">
       <FaIceCream className="text-4xl text-pink-600" />
       <SignupForm />
     </main>
